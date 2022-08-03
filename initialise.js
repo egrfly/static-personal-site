1de11fbf33983fa7632b0d9633c39b0db5f3a30d
egrfly <emily.g.f@outlook.com>
2022-08-03 09:29:12 +0100

    Create CNAME

--- initialise.js
+++ initialise.js
@@ -2,13 +2,6 @@ var themeButton = document.querySelector("footer button");
 var themeButtonText = document.querySelector("footer button strong");
 var themeButtonIcon = document.querySelector("footer button img");
 
-// If the user prefers dark mode, switch to dark mode on page load
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.body.classList.add("dark");
-  themeButtonText.innerText = "Switch to light mode";
-  themeButtonIcon.src = "icons/sun-solid.svg";
-}
-
 // Allow the user to toggle dark mode (won't persist beyond refresh)
 themeButton.style.display = "inline-flex";
 themeButton.addEventListener("click", function() {
